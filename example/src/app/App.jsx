f457226ebf149eedab89d200e1d5268c2cae9133
kalle <linsenboll@cleverbridge.com>
2016-01-06 00:49:17 +0100

    - icons for file types (some)

--- example/src/app/App.jsx
+++ example/src/app/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './components/main.jsx';
+import Main from './components/Main.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 //
