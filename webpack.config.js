b9a15b588037c7a84570bf62a73808e35abd02a6
kalle <linsenboll@cleverbridge.com>
2016-06-24 18:08:54 +0200

    webpack config listens to package json scripts commands

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,15 @@
 /**
  * Created by kalle on 04.01.2016.
  */
-var path = require('path');
-var webpack = require('webpack');
+const Path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
 
-module.exports = {
-    entry: [
-        'webpack/hot/dev-server',
-        path.resolve(__dirname, 'example/src/app/app.jsx')
-    ],
-    output: {
-        path: path.resolve(__dirname, 'example/www'),
-        //publicPath: 'http://localhost:8001/example',
-        filename: 'example.js'
-    },
-    plugins:[
-        /*new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })*/
+const Common = {
+    plugins: [
+        //new HtmlWebpackPlugin({
+        //    title: 'Webpack demo'
+        //})
     ],
     module: {
         loaders: [
@@ -32,10 +22,36 @@ module.exports = {
                 }
             }
         ]
-    },
-    externals: {
-        //don't bundle the 'react' npm package with our bundle.js
-        //but get it from a global 'React' variable
-        //'react': 'React'
     }
 };
+
+let config;
+// Detect how npm is run and branch based on that
+switch (process.env.npm_lifecycle_event) {
+    case 'build':
+        config = merge(Common, {
+            entry: [
+                Path.resolve(__dirname, 'src/FileStorage.jsx')
+            ],
+            output: {
+                path: Path.resolve(__dirname, 'lib'),
+                filename: 'file_storage.js'
+            }
+        });
+        break;
+    default:
+        config = merge(Common, {
+            entry: [
+                'webpack/hot/dev-server',
+                Path.resolve(__dirname, 'example/src/app/app.jsx'),
+            ],
+            output: {
+                path: Path.resolve(__dirname, 'lib'),
+                //publicPath: 'http://localhost:8001/example',
+                filename: 'example.js'
+            }
+        });
+        break;
+}
+
+module.exports = config;
